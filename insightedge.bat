186d37d4dff6e33475c07b39f280131d431262fb
Niv Ingberg <niv@gigaspaces.com>
2017-11-15 14:29:13 +0200

    Enhanced build to copy dependencies

--- insightedge.bat
+++ insightedge.bat
@@ -1,3 +1,2 @@
 @echo off
-rem java -jar target\cli-test-1.0-SNAPSHOT.jar org.niv.Program %*
-java -cp target\classes;deps\*; org.niv.Program %*
+java -cp target\*;target\dependencies\*; org.niv.Program %*
